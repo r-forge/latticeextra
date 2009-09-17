047681ec83cb058e796f5520e7223bee1a4691a3
felix (Felix Andrews) <felix@nfrac.org>
2009-09-17 07:26:50 +0000

    new: panel.xyarea, panel.qqmath.approx
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/latticeextra@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/panel.xyarea.R
@@ -0,0 +1,59 @@
+##
+## Copyright (c) 2008 Felix Andrews <felix@nfrac.org>
+##
+
+
+## Plot a series as a filled polygon connected at given origin (on y axis).
+## With groups, acts like panel.superpose, but with polygon style settings.
+panel.xyarea <-
+    function(x, y, groups = NULL, origin = NULL,
+             col = if (is.null(groups)) plot.polygon$col else superpose.polygon$col,
+             border = if (is.null(groups)) plot.polygon$border else superpose.polygon$border,
+             lty = if (is.null(groups)) plot.polygon$lty else superpose.polygon$lty,
+             lwd = if (is.null(groups)) plot.polygon$lwd else superpose.polygon$lwd,
+             alpha = if (is.null(groups)) plot.polygon$alpha else superpose.polygon$alpha,
+             ..., panel.groups = panel.xyarea)
+{
+    plot.polygon <- trellis.par.get("plot.polygon")
+    superpose.polygon <- trellis.par.get("superpose.polygon")
+    if (!is.null(groups)) {
+        panel.superpose(x, y, ..., groups = groups, panel.groups = panel.groups,
+                        col = col, border = border, lty = lty, lwd = lwd,
+                        alpha = alpha, origin = origin)
+    } else {
+        xx <- c(head(x,1), x, tail(x,1))
+        if (is.null(origin))
+            origin <- current.panel.limits()$ylim[1]
+        yy <- c(origin, y, origin)
+        panel.polygon(xx, yy, alpha = alpha, col = col, border = border, lty = lty, lwd = lwd, ...)
+    }
+}
+
+## A slightly modified copy of panel.qqmath
+panel.qqmath.xyarea <-
+    function(x, f.value = NULL, distribution = qnorm, qtype = 7,
+             groups = NULL, ...)
+{
+    x <- as.numeric(x)
+    distribution <- if (is.function(distribution))
+        distribution
+    else if (is.character(distribution))
+        get(distribution)
+    else eval(distribution)
+    nobs <- sum(!is.na(x))
+    if (!is.null(groups))
+        panel.xyarea(x, y = NULL, f.value = f.value, distribution = distribution,
+                     qtype = qtype, groups = groups, ...,
+                     panel.groups = panel.qqmath.xyarea)
+    else if (nobs) {
+        if (is.null(f.value))
+            panel.xyarea(x = distribution(ppoints(nobs)), y = sort(x),
+                ...)
+        else panel.xyarea(x = distribution(if (is.numeric(f.value))
+            f.value
+        else f.value(nobs)), y = quantile(x, if (is.numeric(f.value))
+            f.value
+        else f.value(nobs), names = FALSE, type = qtype, na.rm = TRUE),
+            ...)
+    }
+}
