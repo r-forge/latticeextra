245f9031056c75de7eac81bb2b61444778a9df00
felix (Felix Andrews) <felix@nfrac.org>
2010-01-07 01:07:06 +0000

    use half the kernel for one-sided smoothing.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/latticeextra@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/panel.tskernel.R
+++ pkg/R/panel.tskernel.R
@@ -2,7 +2,7 @@
 panel.tskernel <-
     function(x, y, ...,
              width, c = 1, sides = 2, circular = FALSE,
-             kern = kernel("daniell", rep(round(0.5*width / sqrt(c)), c)))
+             kern = kernel("daniell", rep(floor((width/sides)/sqrt(c)), c)))
 {
     if (!missing(y)) {
         if (diff(range(diff(x))) > getOption("ts.eps"))
@@ -10,6 +10,10 @@ panel.tskernel <-
         x <- ts(y, start = x[1], end = tail(x,1), deltat = diff(x[1:2]))
     }
     x <- as.ts(x)
-    i <- -kern$m:kern$m
+    if (sides == 2) {
+        i <- -kern$m:kern$m
+    } else if (sides == 1) {
+        i <- -kern$m:0
+    } else stop("unrecognised value of 'sides'")
     panel.lines(filter(x, kern[i], sides = sides, circular = circular), ...)
 }
